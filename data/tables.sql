0322e62e44a09d5250e2620f170dd26f249159ab
Olivér <oliver.mrakovics@gmail.com>
2022-02-05 09:58:36 +0100

    added users table

--- data/tables.sql
+++ data/tables.sql
@@ -10,3 +10,10 @@ CREATE TABLE passwords(
     favorite BOOLEAN DEFAULT FALSE,
     user_id INT(255) DEFAULT 1
 );
+
+CREATE TABLE users(
+	id INT(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(16) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
